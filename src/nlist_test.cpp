3756288145e8d57e9596ddd90a7cc757627bbfcd
Steve Sherman <stevegsherman@gmail.com>
2014-07-23 19:47:02 -0400

    added variable radius testing to unit tests

--- src/nlist_test.cpp
+++ src/nlist_test.cpp
@@ -5,69 +5,139 @@
 #include "utilities.h"
 #include <cuda_runtime.h>
 
+class BasicPsystem{
 
-TEST(NlistTest, SimpleCuda) {
-	uint* nlist;
-	ASSERT_EQ(cudaMalloc((void**) &nlist, 2*sizeof(float)), cudaSuccess);
-	ASSERT_EQ(cudaFree(nlist), cudaSuccess);
-}
+public:
+	NewParams hp;
 
-
-TEST(NlistTest, twop_adjacent_resize)
-{
-	NewParams host;
-	host.L = make_float3(10,10,10);
-	host.Linv = 1/host.L;
-	host.N = 2;
-	host.numAdjCells = 1;
-	uint *dnlist, *dnumneigh;
-	uint nlist_size = 0;
-
-	cudaMalloc(&dnumneigh, 2*sizeof(uint));
-	uint* phash;
-	cudaMalloc(&phash, sizeof(uint)*2);
-	cudaMemset(phash,0, 2*sizeof(uint));
-
+	uint *dNList;
+	uint nListSize;
+	uint *dNumNeigh;
 	float4* dpos;
-	float4 hpos[2] = {make_float4(0,0,0,1), make_float4(2,0,0,1)};
-	cudaMalloc( &dpos, 2*sizeof(float4));
-	cudaMemcpy(dpos,hpos,sizeof(float4)*host.N,cudaMemcpyHostToDevice);
+	uint* phash;
 
-	uint* cellStart, *cellEnd;
-	cudaMalloc( &cellStart, sizeof(int));
-	cudaMalloc( &cellEnd, sizeof(int));
-	int st = 0, end = 2;
-	cudaMemcpy(cellStart, &st, sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemcpy(cellEnd, &end, sizeof(int), cudaMemcpyHostToDevice);
-
+	uint* cellStart;
+	uint* cellEnd;
 	uint* cellAdj;
-	cudaMalloc(&cellAdj, sizeof(uint));
-	cudaMemset(cellAdj, 0, sizeof(uint));
+
+	bool setDPos(float4* in_pos) {
+		return cudaMemcpy(dpos, in_pos, sizeof(float4)*hp.N, cudaMemcpyHostToDevice) == cudaSuccess;
+	}
+
+	bool allocNList(uint sz) {
+		nListSize = sz;
+		bool flag = cudaMalloc(&dNList, hp.N*nListSize*sizeof(uint)) == cudaSuccess;
+		cudaMemset(&dNList, 0, nListSize*hp.N*sizeof(uint));
+		return flag;
+	}
+
+	void setDevParams() { setNParameters(&hp); }
+
+};
+
+class TwoPTest : public BasicPsystem, public ::testing::TestWithParam<float> {
+public:
+	TwoPTest() {
+		hp.N = 2;
+		hp.L = make_float3(100,100,100); hp.Linv = 1/hp.L;
+		hp.numAdjCells = 1;
+		setDevParams();
+
+		nListSize = 0;
+		dNList = NULL;
+
+		cudaMalloc(&dNumNeigh, hp.N*sizeof(uint));
+		cudaMemset(&dNumNeigh,0, hp.N*sizeof(uint));
+
+		cudaMalloc(&dpos, hp.N*sizeof(float4));
+		cudaMalloc(&phash, hp.N*sizeof(uint));
+		cudaMemset(phash,0, hp.N*sizeof(uint));
+
+		// Everything is in one cell, so NOT a test of cell placement
+		cudaMalloc( &cellStart, sizeof(int));
+		cudaMalloc( &cellEnd, sizeof(int));
+		cudaMalloc( &cellAdj, sizeof(uint));
+		uint st = 0, end = 2;
+		cudaMemcpy(cellStart, &st, sizeof(uint), cudaMemcpyHostToDevice);
+		cudaMemcpy(cellEnd, &end, sizeof(uint), cudaMemcpyHostToDevice);
+		cudaMemset(cellAdj, 0, sizeof(uint));
+	}
+
+	~TwoPTest(){
+		cudaFree(dNList);
+		cudaFree(dNumNeigh);
+		cudaFree(dpos);
+		cudaFree(phash);
+		cudaFree(cellStart);
+		cudaFree(cellEnd);
+		cudaFree(cellAdj);
+	}
+};
 
 
-	setNParameters(&host);
-	uint maxn = funcNList(dnlist, dnumneigh, (float*) dpos, phash, cellStart,
-			cellEnd, cellAdj, host.N, nlist_size,VarCond(1.1));
+TEST_F(TwoPTest, VarCondBasic) {
+	float4 fakepos[2] = {make_float4(0,0,0,1), make_float4(2,0,0,1)};
+	ASSERT_TRUE(setDPos(fakepos));
+
+	//sepdist is 2, distance is 2
+	uint maxn = funcNList(dNList, dNumNeigh,(float*) dpos, phash,
+			cellStart,cellEnd,cellAdj, hp.N, nListSize, VarCond(1.01));
 	ASSERT_EQ(maxn,1);
 
-
-	maxn = funcNList(dnlist, dnumneigh, (float*) dpos, phash, cellStart,
-				cellEnd, cellAdj, host.N, nlist_size,VarCond(0.9));
+	maxn = funcNList(dNList, dNumNeigh,(float*) dpos, phash,
+			cellStart,cellEnd,cellAdj, hp.N, nListSize, VarCond(0.99));
 	ASSERT_EQ(maxn,0);
-
-
-	cudaFree(dpos);
-	cudaFree(dnlist);
-	cudaFree(dnumneigh);
-	cudaFree(phash);
-	cudaFree(cellStart);
-	cudaFree(cellEnd);
-	cudaFree(cellAdj);
 }
 
+TEST_F(TwoPTest, VarCondFar) {
+	float4 fakepos[2] = {make_float4(0,0,0,1), make_float4(4,0,0,1)};
+	ASSERT_TRUE(setDPos(fakepos));
 
-/*
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}*/
+	//var cond identifies points with dist < param*(a1+a2);
+	//sepdist is 2, distance is 4
+	EXPECT_EQ(0, funcNList(dNList, dNumNeigh,(float*) dpos, phash,
+			cellStart,cellEnd,cellAdj, hp.N, nListSize, VarCond(1.01)));
+
+	//exactly equal, so NOT neighbors
+	EXPECT_EQ(0, funcNList(dNList, dNumNeigh,(float*) dpos, phash,
+			cellStart,cellEnd,cellAdj, hp.N, nListSize, VarCond(2.0)));
+
+	EXPECT_EQ(1, funcNList(dNList, dNumNeigh,(float*) dpos, phash,
+			cellStart,cellEnd,cellAdj, hp.N, nListSize, VarCond(2.01)));
+}
+
+TEST_P(TwoPTest, VarCondRadius) {
+	float4 fakepos[2] = {make_float4(0,0,0,1.0),
+			make_float4(1.0f+GetParam(),0,0,GetParam() )};
+	ASSERT_TRUE(setDPos(fakepos));
+
+	EXPECT_EQ(0,funcNList(dNList, dNumNeigh,(float*) dpos, phash,
+			cellStart,cellEnd,cellAdj, hp.N, nListSize, VarCond(0.99f)));
+
+	//exactly equal, so NOT neighbors but to floating prec?
+	EXPECT_EQ(0, funcNList(dNList, dNumNeigh,(float*) dpos, phash,
+			cellStart,cellEnd,cellAdj, hp.N, nListSize, VarCond(1.0f)));
+
+	EXPECT_EQ(1, funcNList(dNList, dNumNeigh,(float*) dpos, phash,
+			cellStart,cellEnd,cellAdj, hp.N, nListSize, VarCond(1.1f)));
+
+	float4 fp2[2] = {make_float4(1.0f+GetParam(),0,0,GetParam() ),
+			make_float4(0,0,0,1.0)};
+	ASSERT_TRUE(setDPos(fp2));
+
+	EXPECT_EQ(0,funcNList(dNList, dNumNeigh,(float*) dpos, phash,
+			cellStart,cellEnd,cellAdj, hp.N, nListSize, VarCond(0.99f)));
+
+	//exactly equal, so NOT neighbors but to floating prec?
+	EXPECT_EQ(0, funcNList(dNList, dNumNeigh,(float*) dpos, phash,
+			cellStart,cellEnd,cellAdj, hp.N, nListSize, VarCond(1.0f)));
+
+	EXPECT_EQ(1, funcNList(dNList, dNumNeigh,(float*) dpos, phash,
+			cellStart,cellEnd,cellAdj, hp.N, nListSize, VarCond(1.1f)));
+}
+
+INSTANTIATE_TEST_CASE_P(NearOne, TwoPTest, ::testing::Range(0.2f,5.0f,0.1f));
+
+TEST_F(TwoPTest, VarCondDiffRadius) {
+
+}
