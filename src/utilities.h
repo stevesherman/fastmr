2e65b629712987f9ea703096ffc53e053f0e3e5e
Steve Sherman <ssherma2@umd.edu>
2014-12-19 12:24:09 -0500

    removed dead code

--- src/utilities.h
+++ src/utilities.h
@@ -26,8 +26,6 @@ void cudaGLInit(int argc, char **argv);
 void allocateArray(void **devPtr, int size);
 void freeArray(void *devPtr);
 
-//void threadSync();
-
 void copyArrayFromDevice(void* host, const void* device, struct cudaGraphicsResource **cuda_vbo_resource, int size);
 void copyArrayToDevice(void* device, const void* host, int offset, int size);
 void registerGLBufferObject(uint vbo, struct cudaGraphicsResource **cuda_vbo_resource);
